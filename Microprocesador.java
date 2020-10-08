8598fa12943831765e8cd56de8a7a5cb6db9cc5c
Angel Heredia <angl.vaz6199@gmail.com>
2020-10-07 20:25:59 -0500

    Segunda versión primera entrega

--- Microprocesador.java
+++ Microprocesador.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class Microprocesador implements Comparable<Microprocesador>{
     private int id;
-    private int duracion; //tiempoTotal
-    private int tb; //block
-    private int tcc; //context
+    private int duracion; // tiempoTotal
+    private int tb; // block
+    private int tcc; // context
     private int quantum;
     private boolean esVacio;
-    private ArrayList<Proceso> completados = new ArrayList<Proceso>(); //terminados
-    
+    private ArrayList<Proceso> completados = new ArrayList<Proceso>(); // terminados
+
     public Microprocesador(int id, int tcc, int quantum, int tb) {
         this.id = id;
         this.duracion = 0;
@@ -18,7 +18,6 @@ public class Microprocesador implements Comparable<Microprocesador>{
         this.esVacio = true;
     }
 
-    
     @Override
     public int compareTo(Microprocesador o) {
         if (this.duracion>o.duracion){
@@ -29,6 +28,64 @@ public class Microprocesador implements Comparable<Microprocesador>{
         }
         return 0;
     }
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDuracion() {
+        return this.duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getTb() {
+        return this.tb;
+    }
+
+    public void setTb(int tb) {
+        this.tb = tb;
+    }
+
+    public int getTcc() {
+        return this.tcc;
+    }
+
+    public void setTcc(int tcc) {
+        this.tcc = tcc;
+    }
+
+    public int getQuantum() {
+        return this.quantum;
+    }
+
+    public void setQuantum(int quantum) {
+        this.quantum = quantum;
+    }
+
+    public boolean isEsVacio() {
+        return this.esVacio;
+    }
+
+    public void setEsVacio(boolean esVacio) {
+        this.esVacio = esVacio;
+    }
+
+    public ArrayList<Proceso> getCompletados() {
+        return this.completados;
+    }
+
+    public void setCompletados(ArrayList<Proceso> completados) {
+        this.completados = completados;
+    }
+    
+    
 
     
 }
--- Proceso.java
+++ Proceso.java
@@ -1,13 +1,13 @@
 public class Proceso {
 
-    private String nombre; //id
-    private int te; //exeTime
-    private int tcc =0;  
-    private int numBloq; //tb
-    private int tiempoEntrada; //entryTime 
+    private String nombre; // id
+    private int te; // exeTime
+    private int tcc = 0;
+    private int numBloq; // tb
+    private int tiempoEntrada; // entryTime
     private int tvc;
-    private int tiempoTotal; //total
-    private int tiempoInicial; //Tinicial
+    private int tiempoTotal; // total
+    private int tiempoInicial; // Tinicial
     private int tiempoFinal;
 
     public Proceso(String nombre, int te, int tiempoEntrada) {
@@ -15,6 +15,77 @@ public class Proceso {
         this.te = te;
         this.tiempoEntrada = tiempoEntrada;
     }
-    
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTe() {
+        return this.te;
+    }
+
+    public void setTe(int te) {
+        this.te = te;
+    }
+
+    public int getTcc() {
+        return this.tcc;
+    }
+
+    public void setTcc(int tcc) {
+        this.tcc = tcc;
+    }
+
+    public int getNumBloq() {
+        return this.numBloq;
+    }
+
+    public void setNumBloq(int numBloq) {
+        this.numBloq = numBloq;
+    }
+
+    public int getTiempoEntrada() {
+        return this.tiempoEntrada;
+    }
+
+    public void setTiempoEntrada(int tiempoEntrada) {
+        this.tiempoEntrada = tiempoEntrada;
+    }
+
+    public int getTvc() {
+        return this.tvc;
+    }
+
+    public void setTvc(int tvc) {
+        this.tvc = tvc;
+    }
+
+    public int getTiempoTotal() {
+        return this.tiempoTotal;
+    }
+
+    public void setTiempoTotal(int tiempoTotal) {
+        this.tiempoTotal = tiempoTotal;
+    }
+
+    public int getTiempoInicial() {
+        return this.tiempoInicial;
+    }
+
+    public void setTiempoInicial(int tiempoInicial) {
+        this.tiempoInicial = tiempoInicial;
+    }
+
+    public int getTiempoFinal() {
+        return this.tiempoFinal;
+    }
+
+    public void setTiempoFinal(int tiempoFinal) {
+        this.tiempoFinal = tiempoFinal;
+    }
    
 }
